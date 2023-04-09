fad2d16be019b33105bb81084c5a50853230e6ad
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-04-09 18:01:23 +0500

    Step 2: Data Access and Client-Server Communication

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,38 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
-	"use strict";
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast",
+  "sap/m/MessageBox",
+  "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, MessageBox, JSONModel) {
+  "use strict";
 
-	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
+  return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
 
-		/**
-		 *  Hook for initializing the controller
-		 */
-		onInit : function () {
-			var oJSONData = {
-					busy : false
-				},
-				oModel = new JSONModel(oJSONData);
+    /**
+     *  Hook for initializing the controller
+     */
+    onInit: function () {
+      var oJSONData = {
+        busy: false
+      },
+        oModel = new JSONModel(oJSONData);
 
-			this.getView().setModel(oModel, "appView");
-		}
-	});
+      this.getView().setModel(oModel, "appView");
+    },
+
+    onRefresh: function () {
+      var oBinding = this.byId("peopleList").getBinding("items");
+
+      if (oBinding.hasPendingChanges()) {
+        MessageBox.error(this._getText("refreshNotPossibleMessage"));
+        return;
+      }
+      oBinding.refresh();
+      MessageToast.show(this._getText("refreshSuccessMessage"));
+    },
+
+    _getText: function (sTextId, aArgs) {
+      return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
+    }
+  });
 });
